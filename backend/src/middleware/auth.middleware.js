86cb728e925b8dad8fce516fd8fa209d16392c01
Petermchikho <petercharlesmchikho1@gmail.com>
2025-10-20 09:31:42 +0200

    user routes added

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,6 @@
+export const protectRoute = (req, res, next) => {
+  if (!req.auth || !req.auth.userId) {
+    return res.status(401).json({ message: 'Unauthorized - you must be logged in' });
+  }
+    next();
+};
